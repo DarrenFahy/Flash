f89c029fd984e3a56bd157114a2f34abd57d0c63
Matt <mrk93@georgetown.edu>
2017-04-10 21:10:16 -0400

    Working on developing FlashCard's MVs.

--- app/src/main/java/cosc150/flash/FlashCard.java
+++ app/src/main/java/cosc150/flash/FlashCard.java
@@ -7,20 +7,39 @@ package cosc150.flash;
 
 public class FlashCard
 {
+    public static final double EIGHT = 8;
     String charString;
     String meaning;
     String pinyin;
     int character;
+    double pastEight;
+    double priority; //the higher the number, the more the user knows the flash card.
 
     public FlashCard( String cs, String p, String m)
     {
         charString = cs;
         pinyin = p;
         meaning = m;
+        pastEightAtempts = 0;
+        priority = 0.0;
 
     }
 
-    public void setCharacter( String s)
+    //getters
+    public double getPastEightAtempts() {return pastEightAtempts;}
+    public String getMeaning() {return meaning;}
+    public String getCharString() {return charString;}
+    public double getPriority() {return priority;}
+    public int getCharacter() {return character;}
+    public String getPinyin() {return pinyin;}
+
+    //setters
+    public void calculatePriority() {priority = getPastEightAtempts()/EIGHT;}
+    public void setPinyin(String pinyin) {this.pinyin = pinyin;}
+    public void incrementPastEightAtempts() {pastEightAtempts++;}
+    public void setCharString(String charString) {this.charString = charString;}
+    public void setMeaning(String meaning) {this.meaning = meaning;}
+    public void setCharacter(String s)
     {
         int i3 = 0x2f20;
         for ( int i=0; i<100; i++ )
